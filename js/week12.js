27efc9d367f4fb71441b1fdab4b008d6b533b8d9
MohamedElSheati <m.elsheati@gmail.com>
2024-01-16 15:58:34 +0200

    second commit

--- js/week12.js
+++ js/week12.js
@@ -1,57 +1,60 @@
-let searchInput=document.getElementById("searchInput")
-let todayName=document.getElementById("today-date-name")
-let todayNumber=document.getElementById("today-date-number")
-let todayMonth=document.getElementById("today-date-month")
-let todayLocation=document.getElementById("today-location")
-let todayTemp=document.getElementById("today-temp")
-let todayConditionImg=document.getElementById("today-condition-img")
-let todayConditionTxt=document.getElementById("today-condition-text")
+"use strict"
+let todayDay=document.getElementById("today")
+let todayNum=document.getElementById("todayNum")
+let todayMonth=document.getElementById("todayMonth")
+let nextDay=document.getElementsByClassName("next-day")
+let cityName=document.getElementById("city")
+let cityTemp=document.getElementById("temp")
+let cityCond=document.getElementById("icon")
+let cityWeatherDesc=document.getElementById("text")
 let humidity=document.getElementById("humidity")
-let wind=document.getElementById("wind")
-let windDirection=document.getElementById("wind-direction")
+let windSpeed=document.getElementById("windSpeed")
+let windDirection=document.getElementById("windDirection")
+let maxTemp=document.getElementsByClassName("max-Temp")
+let minTemp=document.getElementsByClassName("min-Temp")
+let nextCondition=document.getElementsByClassName("cond")
+let nextCondIcon=document.getElementsByClassName("next-img")
+let search=document.querySelector("#search")
+let find=document.querySelector("#button-addon1")
 
-let nextDay=document.getElementById("next-date-name")
-let nextMaxTemp=document.getElementById("next-max-temp")
-let nextMinTemp=document.getElementById("next-min-temp")
-let nextConiditonImg=document.getElementById("next-coniditon-img")
-let nextConditionTxt=document.getElementById("next-condition-text")
+let dataWeather=[]
+let forecastData=[]
 
-async function getWeatherData(){
-    let weatherResponse =await fetch("http://api.weatherapi.com/v1/forecast.json?key=f3b6cacf02c74d6a8c5141335241101&q=07112&days=7")
-    let weatherData=weatherResponse.json()
-    return weatherData
-}
 
-function displayTodayDate(date){
-    todayLocation.innerHTML=date.location.name
-    todayTemp.innerHTML=date.current.temp_c
-    todayConditionImg.setAttribute("src",date.current.condition.icon)
-    todayConditionTxt.innerHTML=date.current.condition.text
-    humidity.innerHTML=date.current.humidity+"%"
-    wind.innerHTML=date.current.wind_kph+"KM/H"
-    windDirection.innerHTML=date.current.wind_dir
-}
+find.addEventListener("click",function(){
+        weatherData(search.value)
+    })
+  
 
-function displayNextData(data){
-    let forecastData=data.forecast.forecastday
-    for(let i0;i<2;i++){
-        let nextDate=new Date(forecastData[i+1].data)
-        nextDay[i].innerHTML=nextDate.toLocaleDateString("en-us",{weekday:"long"})
-        nextMaxTemp[i].innerHTML=forecastData[i+1].day.maxtemp_c
-        nextMinTemp[i].innerHTML=forecastData[i+1].day.mintemp_c
-        nextConiditonImgConditionImg.setAttribute("src",forecastData[i+1].day.condition.icon)
-        nextConditionTxt.innerHTML=forecastData[i+1].day.condition.text
+
+async function weatherData(name){
+    let req= await fetch(`https://api.weatherapi.com/v1/forecast.json?key=f3b6cacf02c74d6a8c5141335241101&q=${name}&days=4`)
+    dataWeather = await req.json()
+    forecastData=dataWeather.forecast.forecastday
+    
+    let todayDate=new Date(dataWeather.current.last_updated)
+    todayDay.innerHTML=todayDate.toLocaleString("en-us",{weekday:"long"})
+    todayNum.innerHTML=todayDate.getDate()
+    todayMonth.innerHTML=todayDate.toLocaleString("en-us",{month:"short"})
+    cityName.innerHTML=dataWeather.location.name;
+    cityTemp.innerHTML=dataWeather.current.temp_c+` <sup>o</sup>C`
+    cityCond.setAttribute("src",dataWeather.current.condition.icon)
+    cityWeatherDesc.innerHTML=dataWeather.current.condition.text
+    humidity.innerHTML=dataWeather.current.humidity+"%"
+    windSpeed.innerHTML=dataWeather.current.wind_kph+"Km/hr"
+    windDirection.innerHTML=dataWeather.current.wind_dir
+    
+    for(let i=0;i<forecastData.length;i++){
+        let nextDate=new Date(forecastData[i+1].date)
+        nextDay[i].innerHTML=nextDate.toLocaleString("en-us",{weekday:"long"})
+        nextCondIcon[i].setAttribute("src",forecastData[i+1].day.condition.icon)
+        maxTemp[i].innerHTML=forecastData[i+1].day.maxtemp_c+` <sup>o</sup>`
+        minTemp[i].innerHTML=forecastData[i+1].day.mintemp_c+` <sup>o</sup>`
+        nextCondition[i].innerHTML=forecastData[i+1].day.condition.text
     }
+    
 }
 
-async function Ready(){
-    let weatherData=await getWeatherData()
-    displayTodayDate(weatherData)
-    displayNextData(weatherData)
-}
 
-Ready()
 
-searchInput.addEventListener("keyup",function(){
-    Ready(searchInput.value)
-})
+
